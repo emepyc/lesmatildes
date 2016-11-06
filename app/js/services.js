a74cb6e944a237083b17a29f8a914363b80d97f8
Miguel Pignatelli <emepyc@gmail.com>
2016-11-06 17:38:54 +0000

    removed items images

--- app/js/services.js
+++ app/js/services.js
@@ -6,14 +6,14 @@ services
     .factory('courses', ['$log', '$http', function ($log, $http) {
         // Reads the courses from the appropriate url and returns a promise with the object.
         $log.log("reading courses file from fs");
-        var coursesPromise = $http.get('/cursos.json');
+        var coursesPromise = $http.get('https://emepyc.github.io/lesmatildes/cursos.json');
         return {
             fetch: coursesPromise
         };
     }])
     .factory('items', ['$log', '$http', function ($log, $http) {
         $log.log("reading items file from fs");
-        var itemsPromise = $http.get('/items.json');
+        var itemsPromise = $http.get('https://emepyc.github.io/lesmatildes/items.json');
         return {
             fetch: itemsPromise
         };
